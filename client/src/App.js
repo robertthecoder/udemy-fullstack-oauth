baf2ce9d84dfdac853b102066862cbe8a629c3e4
Robert Kim <rkim-mits@gmail.com>
2020-07-13 11:33:37 -0700

    add frontend, still need to setup separate dev and prod mongodb

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Node FUll Stack w/ React:</h2>
+        <a href="/auth/google">
+          O Auth - Sign in with Google
+        </a>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
