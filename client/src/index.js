e96c959c92ff9184f88ffe91cd111a7591e9b5b8
Robert Kim <rkim-mits@gmail.com>
2020-07-14 09:30:46 -0700

    add routing and redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,23 @@
 // index.js - data layer control (redux)
 // app.js - react router layer
 
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+
 import React from "react";
 
 import ReactDOM from "react-dom";
 
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+import reducers from "./reducers";
+
+// This is where we pass in reducers
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
